73ebea21900ba3acf7c5b14fdcedfdc7c0a2f67e
smcake <smcake@naver.com>
2020-04-08 23:36:30 +0900

    Main화면 UI구성

---
+++ Architecture/app/src/main/java/com/smcake/architecture/tasks/TasksActivity.kt
@@ -0,0 +1,18 @@
+package com.smcake.architecture.tasks
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smcake.architecture.R
+
+/**
+ * Created by JJH on 2020-04-08
+ */
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_act)
+
+
+    }
+}
