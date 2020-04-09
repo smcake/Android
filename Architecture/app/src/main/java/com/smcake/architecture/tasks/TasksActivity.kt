efc4327c8ba82aea5240444e12fa09a53c51773d
smcake <smcake@naver.com>
2020-04-09 22:26:00 +0900

    Main화면 UI구성 기본 동작 처리

--- Architecture/app/src/main/java/com/smcake/architecture/tasks/TasksActivity.kt
+++ Architecture/app/src/main/java/com/smcake/architecture/tasks/TasksActivity.kt
@@ -1,8 +1,12 @@
 package com.smcake.architecture.tasks
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import com.smcake.architecture.R
+import kotlinx.android.synthetic.main.tasks_act.*
 
 /**
  * Created by JJH on 2020-04-08
@@ -13,6 +17,49 @@ class TasksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tasks_act)
 
+        setToolbar()
+        setupNavigationDrawer()
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    private fun setupNavigationDrawer() {
+        drawer_layout.setStatusBarBackground(R.color.colorPrimaryDark)
+        nav_view?.let {
+            setupDrawerContent(it)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawer_layout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupDrawerContent(navigationView : NavigationView) {
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.list_navigation_menu_item -> {
+                }
+                R.id.statistics_navigation_menu_item -> {
+                }
+                else -> {
+                }
+            }
+            it.isChecked = true
+            drawer_layout.closeDrawers()
+            true
+        }
 
     }
 }
